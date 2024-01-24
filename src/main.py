36fd4c0344062c18eccd21dee4e2189ed0ea2a7c
szymiyay <szymoon09@gmail.com>
2024-01-24 22:33:34 +0100

    Add device creation and deletion functionality

--- src/main.py
+++ src/main.py
@@ -101,5 +101,48 @@ app.include_router(user_router.router, tags=["Users"])
 app.include_router(measurement_router.router, tags=["Measurements"])
 
 
+# import paho.mqtt.client as mqtt
+# import ssl
+
+# def on_connect(client, userdata, flags, rc):
+#     print(f"Connected with result code {rc}")
+#     topic = "$iothub/device1/messages/devicebound/#"
+#     mqtt_client.subscribe(topic)
+
+# def on_subscribe(client, userdata, mid, granted_qos):
+#     print(f"Subscribed with QoS: {granted_qos}")
+
+# def on_message(client, userdata, msg):
+#     print(f"Received message: {msg.payload}")
+
+# def on_publish(client, userdata, mid):
+#     print(f"Message published with id: {mid}")
+
+# def on_disconnect(client, userdata, rc):
+#     if rc != 0:
+#         print(f"Unexpected disconnection: {rc}\n")
+#         print(" ")
+
+
+# mqtt_client = mqtt.Client(client_id='device1', protocol=mqtt.MQTTv311)
+# mqtt_client.on_connect = on_connect
+# mqtt_client.on_subscribe = on_subscribe
+# mqtt_client.on_message = on_message
+# mqtt_client.on_publish = on_publish
+# mqtt_client.on_disconnect = on_disconnect
+# mqtt_client.username_pw_set(username="IOTprojekt.azure-devices.net/device1/?api-version=2021-04-12", password="SharedAccessSignature sr=IOTprojekt.azure-devices.net%2Fdevices%2Fdevice1&sig=fcIQpH8Vo6eRSFH5sO80tUjFcojToevYxlJtKuEQAWs%3D&se=1708636469")
+
+# mqtt_client.tls_set(ca_certs=r"C:\Users\szymo\Desktop\Studia\Semestr 5\IOT\iot-api-v2\cert.cer", certfile=None, keyfile=None,
+#                cert_reqs=ssl.CERT_REQUIRED, tls_version=ssl.PROTOCOL_TLSv1_2, ciphers=None)
+# mqtt_client.tls_insecure_set(True)
+
+# mqtt_client.connect("IOTprojekt.azure-devices.net", port=8883)
+# mqtt_client.loop_start()
+
+# mqtt_client.publish("devices/device1/messages/events/", '{"id":123}', qos=1)
+
+# mqtt_client.loop_forever()
+
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
